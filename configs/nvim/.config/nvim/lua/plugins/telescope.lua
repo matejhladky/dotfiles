9223c094d3061047543693b1f5dedbc9145027f3
mathladky <mat.hladky@gmail.com>
2025-02-22 22:02:41 +0100

    more neovim reset

---
+++ configs/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
+
+    -- Setup Telescope with default settings
+    telescope.setup({})
+
+    -- Keymaps for Telescope
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+  end,
+}
+
