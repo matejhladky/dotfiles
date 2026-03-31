8dc5b57f03a4315766d629ffccb6a41137fa1a1f
Matej Hladky <mat.hladky@gmail.com>
2026-03-31 10:35:03 +0200

    update alacritty font and nvim scheme/highlighting

--- configs/nvim/.config/nvim/lua/plugins.lua
+++ configs/nvim/.config/nvim/lua/plugins.lua
@@ -1,10 +1,17 @@
 return {
   {
+    "silentium-theme/silentium.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("silentium")
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "query" },
+        ensure_installed = { "lua", "vim", "vimdoc", "query", "python" },
         highlight = { enable = true },
         indent = { enable = true },
       })
